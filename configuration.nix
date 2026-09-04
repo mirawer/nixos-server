7e2ea5e9a40133d6a66afdb101851d947d45c322
mirawer <michaljanwernik@gmail.com>
2026-09-04 10:20:49 +0200

    Groups, neked Jellyfin and small changes

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,9 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "pl";
 
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
+  documentation.nixos.enable = false;
+
   environment.systemPackages = with pkgs; [
     vim
     wget
@@ -36,7 +39,7 @@
 
   users.users.echo = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "media"];
     hashedPasswordFile = "/var/lib/secrets/echo.pass";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmN5fnFpjehHkQ2+iJhZvZy3gajCvex8iuFLdNP4ZX7 echo@server"
@@ -55,7 +58,8 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 ];
+    allowedTCPPorts = [ 22 8096 ];
+    allowedUDPPorts = [ 7359 ];
   };
 
   #  --  --  Optimalization  --  --
@@ -80,5 +84,27 @@
   boot.tmp.cleanOnBoot = true;
 
   services.journald.extraConfig = "SystemMaxUse=500M";
+
+  #  --  --  Media  --  --
+
+  users.groups.media = { };
+
+  systemd.tmpfiles.rules = [
+    #If it exists ten z if not then build with d	
+
+    "d /srv/media 0755 root root - -"
+    "d /srv/media/films 2750 echo media - -"
+    "d /srv/media/series 2750 echo media - -"
+    
+    "z /srv/media 0755 root root - -"
+    "z /srv/media/films 2750 echo media - -"
+    "z /srv/media/series 2750 echo media - -"
+  ];
+
+  #  --  --  Jellyfin  --  --
+  services.jellyfin.enable = true;
+  
+  users.users.jellyfin.extraGroups = [ "media" ];
+
 
 }
